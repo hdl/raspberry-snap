67f5c68650d2dfda1b12d4037fb2656e2edb715d
Your Name <me@instvpn.net>
2017-02-09 14:27:13 +0800

    good

--- html.sh
+++ html.sh
@@ -4,6 +4,7 @@ NAME=`"cpuinfo"`
 echo "Content-type:text/html\r\n"
 echo "<html><head>"
 echo "<title>$NAME</title>"
+echo '<meta name="viewport" content="width=device-width, initial-scale=1">'
 echo '<meta name="description" content="'$NAME'">'
 echo '<meta name="keywords" content="'$NAME'">'
 echo '<meta http-equiv="Content-type"
@@ -11,10 +12,10 @@ content="text/html;charset=UTF-8">'
 echo '<meta name="ROBOTS" content="noindex">'
 echo "</head><body><pre>"
 ssh -p 20000 pi@localhost '~/snap/take_picture.sh'
-scp -p 20000 pi@localhost:~/snap/pic* .
+scp -P 20000 pi@localhost:~/pic* /home/product/raspberry-snap/
 cat /home/product/raspberry-snap/picdate.txt
 echo "\n"
-
+echo '<img src="pic.jpg" alt="Smiley face" " height="600">'
 echo "\ncpuinfo"
-cat /proc/cpuinfo
+ssh -p 20000 pi@localhost 'cat /proc/cpuinfo'
 echo "</pre></body></html>"
